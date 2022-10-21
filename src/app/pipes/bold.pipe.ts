c5696e23efbe143443f8d6f7adcf933ade859d71
Abdullah Mansoor <mansoorabdullah001@gmail.com>
2022-10-22 03:14:03 +0530

    Data Updated

--- src/app/pipes/bold.pipe.ts
+++ src/app/pipes/bold.pipe.ts
@@ -7,7 +7,7 @@ export class BoldPipe implements PipeTransform {
   transform(value: string): string | string[] {
     if (value.startsWith('<b>')) {
       const str1 = value.substring(3, value.indexOf('</b>'));
-      const str2 = value.substring(value.indexOf('</b>') + 4, value.length - 1);
+      const str2 = value.substring(value.indexOf('</b>') + 4, value.length);
       return [str1, str2];
     } else {
       return value;
